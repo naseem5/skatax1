26d87a3b1fe121363a710b6fa42fe6fa936f3a6a
naseem5 <naseem.siddiqui5@gmail.com>
2011-09-26 22:31:31 +0500

    update

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,11 +1,27 @@
 require 'spec_helper'
 
 describe "LayoutLinks" do
-  describe "GET /layout_links" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get layout_links_index_path
-      response.status.should be(200)
+
+    it "should have a Home page at '/'" do
+      get '/'
+      #response.status.should be(200)
+      response.should have_selector('title', :content=> "S.K. Associates | Home")
     end
-  end
+    
+    it "should have a About page at '/about'" do
+      get '/about'
+      #response.status.should be(200)
+      response.should have_selector('title', :content=> "S.K. Associates | About")
+    end
+    
+    it "should have a Service page at '/service'" do
+      get '/service'
+      #response.status.should be(200)
+      response.should have_selector('title', :content=> "S.K. Associates | Service")
+    end
+    it "should have a Contact page at '/contact'" do
+      get '/contact'
+      #response.status.should be(200)
+      response.should have_selector('title', :content=> "S.K. Associates | Contact")
+    end
 end
